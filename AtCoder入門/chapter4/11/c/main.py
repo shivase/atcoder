bfbb464523e8631c7893045ab1399e168fd94904
shivase <shivase@ymail.ne.jp>
2023-02-27 12:30:15 +0900

    added

---
+++ AtCoder入門/chapter4/11/c/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import math
+
+def main():
+    N, Y = map(int, input().split())
+    x, y, z = -1, -1 , -1
+
+    for a in range(N+1):
+        for b in range(N+1):
+            c = N - a - b
+            if c < 0 or  N < c:
+                continue
+            if a * 10000 + b * 5000 + c * 1000 == Y:
+                x, y, z = a, b, c
+
+
+    print(x,y,z)
+
+if __name__ == '__main__':
+    main()
